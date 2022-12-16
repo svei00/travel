c1c4133dbc0a146b0c3840223f74deda18c7a773
svei00 <79055634+svei00@users.noreply.github.com>
2022-12-15 20:01:04 -0800

    instaled emailjs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,38 @@
 import './contactForm.css';
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from './Button';
 import { RiMailSendLine } from 'react-icons/ri';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
+
+    const refForm = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                'svei00',
+                'template_f2vnjd2',
+                refForm.current,
+                'Ff-0XRmnMhpWt9tDR'
+            )
+            .then (
+                () => {
+                    alert('Message Successfully sent!!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again.')
+                }
+            )
+    }
+    
   return (
     <div className='container'>
         <h1>Contact Us</h1>
-        <form>
+        <form ref={refForm} onSubmit={sendEmail}>
             <input placeholder='Write your Name'/>
             <input placeholder='What is your Email?'/>
             <input placeholder='Subject '/>
